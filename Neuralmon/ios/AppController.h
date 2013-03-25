220424bd050bfd69f68400bfbdfde7f3a3906540
Jenny Valdez <jedaniv@gmail.com>
2013-03-25 16:41:30 -0430

    Initial Commit

---
+++ Neuralmon/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  NeuralmonAppController.h
+//  Neuralmon
+//
+//  Created by Jenny Valdez on 25/03/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Neuralmon/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  NeuralmonAppController.h
+//  Neuralmon
+//
+//  Created by Jenny Valdez on 25/03/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
